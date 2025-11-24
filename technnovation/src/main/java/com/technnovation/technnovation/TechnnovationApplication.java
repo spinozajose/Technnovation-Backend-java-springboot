61e726d33c5b0ddb073fb54d832faaa911dd5317
spinozajose <josi.espinoza@duocuc.cl>
2025-11-23 21:43:30 -0300

    Primer commit, Inicio del proyecto.

---
+++ technnovation/src/main/java/com/technnovation/technnovation/TechnnovationApplication.java
@@ -0,0 +1,13 @@
+package com.technnovation.technnovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnnovationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnnovationApplication.class, args);
+	}
+
+}
---
+++ technnovation/src/test/java/com/technnovation/technnovation/TechnnovationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technnovation.technnovation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnnovationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
